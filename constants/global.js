9c817f0fbd5178000a191a9d301ae1c0c76ae574
Channa <bou.channa93@gmail.com>
2016-04-04 11:58:13 +0700

    config dynamically get ip of server

--- constants/global.js
+++ constants/global.js
@@ -1,6 +1,6 @@
 //const IP_SERVER = "http://192.168.1.244"; //local
 //const IP_SERVER = "http://192.168.1.146"; // server
-var IP_SERVER = "http://192.168.1.146";
+const IP_SERVER = window.location.origin;  // getting ip of server 
 const CARET = '<span class="caret"></span>';
 const URL_WEBSERVICE = IP_SERVER+"/obs/api/"; // url end point will be used later to retrieve data from web service just need controller and funtion name from select option
 const URL_FUNCS = IP_SERVER+"/obs/admin/testFunction/TestFunction/get_funcs";
